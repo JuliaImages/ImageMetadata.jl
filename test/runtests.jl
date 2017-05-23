1fdd355c47b73eaf45a08ddb38d38ee503279199
Tim Holy <tim.holy@gmail.com>
2017-05-23 08:05:30 -0500

    Improve ambiguity detection
    We were getting failures due to ambiguities in StatsBase.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,18 @@
-using ImageMetadata
+using Colors, ColorVectorSpace, FixedPointNumbers, StatsBase, ImageCore, ImageAxes, IndirectArrays
 using Base.Test
 
-if VERSION < v"0.6.0-dev"
-    @test isempty(detect_ambiguities(ImageMetadata,ImageAxes,ImageCore,IndirectArrays,Base,Core))
+mods0 = (Colors, ColorVectorSpace, FixedPointNumbers, StatsBase, ImageCore, ImageAxes, IndirectArrays, Base, Core)
+ambs0 = detect_ambiguities(mods0...)
+
+using ImageMetadata
+ambs = setdiff(detect_ambiguities(ImageMetadata, mods0...), ambs0)
+if !isempty(ambs)
+    println("Ambiguities:")
+    for a in ambs
+        println(a)
+    end
 end
+@test isempty(ambs)
 
 include("core.jl")
 include("operations.jl")
